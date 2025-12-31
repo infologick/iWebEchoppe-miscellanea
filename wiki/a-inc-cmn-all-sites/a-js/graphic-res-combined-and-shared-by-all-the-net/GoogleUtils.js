4afcd08f6e9e7fba4bc4f6220475ed020d896ab6
infologick <eric.moutie@laposte.net>
2025-12-31 17:03:24 +0100

    ajout: pour traduire les pages *.html via google-translation

---
+++ wiki/a-inc-cmn-all-sites/a-js/graphic-res-combined-and-shared-by-all-the-net/GoogleUtils.js
@@ -0,0 +1,49 @@
+/*
+ * Lib. qui permet de gérer dynamiquement les URLs [o_footer] i.e. celles de CrazyStat et Facebook.
+ */
+
+
+
+var GoogleUtils = {
+
+	/**
+	* Vérifie si nom de domaine se termine en .dev ou @int (mode développement), alors pas d'i18n.
+	* @return: bool valant false, quand hôte local pour dév.
+	*/
+	isHostRemoteWhenTrue_versus_localWhenFalse_toSwitchGoogleTranslationOnOrOff() {
+// ~~NOK: ssi forcer test en dév.~~ return true;
+		const sHostname = window.location.hostname;
+		// Vérifie si le domaine se termine par ".com"
+		const bIsComDomain = sHostname.endsWith('.com');
+		// Vérifie si le domaine se termine par un @int
+		const bEndsWithInteger = /\.\d+$/.test(sHostname);
+		// Commercial…
+		if (bIsComDomain) {
+			return true;
+		}
+		// Lookback…
+		if (bEndsWithInteger) {
+			return false;
+		}
+		// .dev est vérifié plus loin
+		return undefined;
+	},
+
+
+
+	googleTranslateElementInit() {
+		if (GoogleUtils.isHostRemoteWhenTrue_versus_localWhenFalse_toSwitchGoogleTranslationOnOrOff()) {
+			new google.translate.TranslateElement({
+				pageLanguage: 'fr',
+				layout: google.translate.TranslateElement.InlineLayout.VERTICAL,
+				}, 'google_translate_element');
+		}
+	}
+
+};
+
+
+/*
+* Go: 1. publication des interfaces aux autres!
+*/
+window.GoogleUtils = GoogleUtils;
